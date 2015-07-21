f7e45d1ac357f65f6dacc5d92003993601e70410
Jay Lubo <undeadlegion@gmail.com>
2015-07-21 10:55:20 -0500

    Initial Commit

---
+++ GoalBuddy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoalBuddy
+//
+//  Created by Jay Lubo on 7/21/15.
+//  Copyright (c) 2015 JLubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoalBuddy/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  GoalBuddy
+//
+//  Created by Jay Lubo on 7/21/15.
+//  Copyright (c) 2015 JLubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
